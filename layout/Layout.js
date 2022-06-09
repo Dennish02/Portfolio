4ee6f989542fb104230216a0593d16ea1adef610
Dennis Nicolas Hesler <dennishesler02@gmail.com>
2022-06-09 18:32:56 -0300

    V1.9

--- layout/Layout.js
+++ layout/Layout.js
@@ -14,7 +14,7 @@ export default function Layout({children}) {
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;700&display=swap" rel="stylesheet"/> 
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"></link>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"/>
 
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"/>
           
@@ -26,7 +26,7 @@ export default function Layout({children}) {
               </main>
              
     </section>
-    <div className='cont-Navbar'>
+            <div className='cont-Navbar'>
                   <Navbar />
 
               </div>
