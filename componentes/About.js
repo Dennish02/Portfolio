4db8575248badd904aa8152a0d27ed2764eb8e45
Dennis <dennishesler02@gmail.com>
2022-08-31 11:05:07 -0300

    arreglo skils

--- componentes/About.js
+++ componentes/About.js
@@ -7,60 +7,44 @@ export default function About() {
       <h3 className="preguntas">Who I am? </h3>
         <p>
           
-          🔸I'm a Full Stack Developer and have the ability
-          to develop projects from both areas like Frontend or
-          Backend, always keeping in mind that codes can be scaled
-          and reused to speed up team development.
+          🔸I'm a Full Stack Developer and have the ability to develop projects from both areas like Front-end or Back-end, always keeping in mind that codes can be scaled and reused to speed up team development.
 
         </p>
         <h3 className="preguntas"> What do I have?</h3>
         <p>
          
-          🔸I am very commit with all activities or projects proposed
-          in a development environment,  I like challenges and eager for
-          new learning to increase my professional knowledge to improving
-          my work and the team where I belong to, I am punctual person and reliable.
-
-          Have experience managing teams, easily adaptable to
-          changes and different work environments and have the ability
-          to resolve situations to be fixed.
+          🔸I am very committed to all activities or projects proposed in a development environment, I like challenges and eager for new learning to increase my professional knowledge to improving my work and the team where I belong to, I am punctual person and reliable. Has experience managing teams, easily adaptable to changes and different work environments and have the ability to resolve situations to be fixed.
 
         </p>
         <h3 className="preguntas">That I can contribute?</h3>
         <p>
           
-          🔸If we have the pleasure of working together,
-          you will know that I always seek to improve the teamwork,
-          building tools that allow facilitate and help all members to carry
-          out an activity more effectively.
-          I consider that I can face every problems like challenges that arise and
-          I know that I can solve  and acomplish them. Additionally Im open minded
-          about to ask for advice, receive corrections and give constructive feedback too
-
+          🔸If we have the pleasure of working together, you will know that I always seek to improve the teamwork, building tools that allow facilitate and help all members to carry out an activity more effectively. I consider that I can face every problem like challenges that arise and I know that I can solve and accomplish them. Additionally, I'm open minded  to ask for advice, receive corrections and give constructive feedback too
         </p>
 
       </article>
       <h2>Technical Skills</h2>
       <div className='TechnicalSkills'>
-        <p>CSS3</p>
-        <p>HTML5</p>
-        <p>React.js</p>
-        <p>Redux</p>
-        <p>Next.js</p>
-        <p>JavaScript</p>
-        <p>PHP</p>
-        <p>SQL</p>
-        <p>MySQL</p>
-        <p>Git</p>
-        <p>Sass</p>
-        <p>Gulp</p>
-        <p>JWT</p>
-        <p>Firebase</p>
-        <p>PostgresSQL</p>
-        <p>Express.js</p>
-        <p>MongoDb</p>
-        <p>Illustrator</p>
-        <p>Photoshop</p>
+        <p>CSS3 <small className="years">+3 years</small></p>
+        <p>HTML5 <small className="years">+3 years</small></p>
+        <p>React.Js <small className="years">1 year</small></p>
+        <p>Redux.Js <small className="years">1 year</small></p>
+        <p>Next.Js <small className="years">1 year</small></p>
+        <p>JavaScript <small className="years">+1 years</small></p>
+        <p>PHP <small className="years">1 year</small></p>
+        <p>SQL <small className="years">1 year</small></p>
+        <p>MySQL <small className="years">1 year</small></p>
+        <p>Git <small className="years">1 year</small></p>
+        <p>Sass <small className="years">+1 years</small></p>
+        <p>Gulp <small className="years">+1 years</small></p>
+        <p>JWT <small className="years">0-1 year</small></p>
+        <p>Firebase <small className="years">0-1 year</small></p>
+        <p>PostgresSQL <small className="years">+3 year</small></p>
+        <p>Express.js <small className="years">1 year</small></p>
+        <p>MongoDb <small className="years">1 year</small></p>
+        <p>Illustrator <small className="years">+2 years</small></p>
+        <p>Photoshop <small className="years">+2 years</small></p>
+        <p>Jest <small className="years">0-1 year</small> </p>
       </div>
       <h2>Soft Skill</h2>
       <div className='TechnicalSkills'>
@@ -79,7 +63,7 @@ export default function About() {
         <p>Docker</p>
         <p>React Native</p>
         <p>GraphQL</p>
-        <p>Testing</p>
+       
         <p>Laravel</p>
       </div>
     </div>
